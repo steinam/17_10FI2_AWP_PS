cad916771d9c3d06f53a4972a74f78dcced39dd8
Karl Steinam <webmaster@steinam.de>
2018-01-25 19:10:40 +0100

    Aufgaben zu Bedingungen und Schleifen

---
+++ woche_3/Schleife.ps1
@@ -0,0 +1,65 @@
+﻿$gesamtlohn=40001
+$tage=1
+
+$tageslohn = 1
+
+while($gesamtlohn -lt 40000)
+{
+    $tageslohn = $tageslohn * 2
+    $gesamtlohn = $gesamtlohn + $tageslohn
+    $tage = $tage + 1
+
+    
+}
+
+
+Write-Host $gesamtlohn
+
+
+##########################
+
+
+$gesamtlohn=1
+$tage=1
+
+$tageslohn = 1
+
+do{
+
+   $tageslohn = $tageslohn * 2
+    $gesamtlohn = $gesamtlohn + $tageslohn
+    $tage = $tage + 1
+
+
+ 
+}while($gesamtlohn -lt 40000)
+
+Write-Host $tage
+Write-Host $gesamtlohn
+
+
+
+$ergebnis = Get-Process
+
+foreach($element in $ergebnis)
+{
+    if($element.ProcessName.Length -lt 10)
+    {
+        Write-Host $element.ProcessName
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ woche_3/prozent_mehrfach.ps1
@@ -0,0 +1,29 @@
+﻿
+$Gesamtpunkte = read-Host "gesamtpunkte"
+$Carolpunkte = Read-Host "Meine Punkte"
+
+
+$prozent = $Carolpunkte * 100 / $Gesamtpunkte
+
+if($prozent -gt 92)
+{
+    Write-Host "Note = 1"
+}
+elseif($prozent -gt 82)
+{
+   Write-Host "Note = 2"
+}
+elseif($prozent -gt 67)
+{
+   Write-Host "Note = 3"
+}
+elseif($prozent -gt 50)
+{
+   Write-Host "Note = 4"
+}
+
+
+#Schulaufgabe: 50 GesamtpunktePunkte möglich
+# Carol hat 34 Punkte
+
+# ==> wiewiel prozent haben sie jetzt
